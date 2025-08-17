2843b3ef663835e4899701a26595d9a540cffede
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2025-08-17 19:04:39 -0300

    Project is ready: 3d animation, animate on scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     const mobileMenu = document.getElementById('mobileMenu')
 
     // If is has the 'hidden' class, remove it otherwise, add it
-    if(mobileMenu.classList.contains('hidden')){
+    if (mobileMenu.classList.contains('hidden')) {
       mobileMenu.classList.remove('hidden');
     } else {
       mobileMenu.classList.add('hidden');
@@ -17,25 +17,45 @@ const Header = () => {
 
   return (
     <header className="flex justify-between items-center py-4 px-4 lg:px-20">
-      <h1 className="text-3xl md:text-4xl lg:text-5xl font-light m-0">
+      <h1
+        data-aos="fade-down"
+        data-aos-easing="linear"
+        data-aos-duration="1500"
+        className="text-3xl md:text-4xl lg:text-5xl font-light m-0">
         MCODE
       </h1>
 
       {/* Desktop Navigation */}
       <nav className="hidden md:flex items-center gap-12">
-        <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
+        <a
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="1000"
+          className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
           COMPANY
         </a>
 
-        <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
+        <a
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="1500"
+          className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
           FEATURES
         </a>
 
-        <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
+        <a
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="2000"
+          className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
           RESOURCES
         </a>
 
-        <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
+        <a
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="2500"
+          className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
           DOCS
         </a>
       </nav>
@@ -53,20 +73,20 @@ const Header = () => {
       <div id='mobileMenu' className='hidden fixed top-16 bottom-0 right-0 left-0 p-5 md:hidden z-40 bg-black opacity-70 backdrop-blur-md'>
         <nav className='flex flex-col gap-6 items-center'>
           <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
-          COMPANY
-        </a>
+            COMPANY
+          </a>
 
-        <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
-          FEATURES
-        </a>
+          <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
+            FEATURES
+          </a>
 
-        <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
-          RESOURCES
-        </a>
+          <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
+            RESOURCES
+          </a>
 
-        <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
-          DOCS
-        </a>
+          <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#">
+            DOCS
+          </a>
         </nav>
       </div>
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
 import 'boxicons/css/boxicons.min.css'
+import Spline from '@splinetool/react-spline'
 
 const Hero = () => {
   return (
     <main className="flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h-[calc(90vh-6rem)]">
-      <div className="max-w-xl ml-[5%] z-10 mt-[90%] md:mt-[60%] lg:mt-0">
+      <div
+        data-aos="fade-right"
+        data-aos-offset="300"
+        data-aos-easing="ease-in-sine"
+        className="max-w-xl ml-[5%] z-10 mt-[90%] md:mt-[60%] lg:mt-0">
         {/* Tag box-with gradient border */}
         <div className='relative w-[95%] sm:w-48 h-10 bg-gradient-to-r from-[#656565] to-[#e99b63] shadow-[0_0_15px_rgba(255,255,255,0.4)] rounded-full'>
           <div className='absolute inset-[3px] bg-black rounded-full flex items-center justify-center gap-1'>
@@ -31,6 +36,17 @@ const Hero = () => {
           <a className='border border-[#2a2a2a] py-2 sm:py-3 px-8 sm:px-10 rounded-full sm:text-lg text-sm font-semibold tracking-wider transition-all duration-300 hover:bg-[#1a1a1a] bg-gray-300 text-black hover:text-white' href="#">Get Started <i className='bx bx-link-external'></i></a>
         </div>
       </div>
+
+      {/* 3D Robot */}
+      <Spline
+        data-aos="fade-zoom-in"
+        data-aos-easing="ease-in-back"
+        data-aos-delay="300"
+        data-aos-offset="0"
+        data-aos-duration="3000"
+        className="absolute lg:top-0 top-[-20%] bottom-0 lg:left-[25%] sm:left-[-2%] h-full"
+        scene="https://prod.spline.design/L98OMIZ8KyIpzFFa/scene.splinecode" />
+
     </main>
   )
 }
